deb1e6c733eef67656e25259229317c3d57818e0
Priya Sharma <priya@smartsolution.dev>
2026-01-22 10:38:19 +0200

    render Idcard component

--- w2-fe-pp-part1/src/App.jsx
+++ w2-fe-pp-part1/src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import IdCard from "./IdCard";
 
 function App() {
   return (
     <div className="App">
-      <h1> LAB | React Training</h1>
+      <IdCard
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={176}
+        birth={new Date("1992-07-14")}  
+    />
+        <IdCard
+      lastName='Delores '
+      firstName='Obrien'
+      gender='female'
+      height={174}
+      birth={new Date("1988-05-11")}
+      picture="https://randomuser.me/api/portraits/women/44.jpg"
+    />
     </div>
   );
 }
---
+++ w2-fe-pp-part1/src/IdCard.jsx
@@ -0,0 +1,22 @@
+
+
+const IdCard = ({ lastName, firstName, gender, height, birth, picture }) => {
+  return (
+    <div className="id-card">
+      <div className="card">
+        <div className="card image"><img src={picture} alt="Profile" /></div>
+        <div>
+        <p><strong>lastName:</strong> {lastName}</p>
+        <p><strong>firstName:</strong> {firstName}</p>
+        <p><strong>Gender:</strong> {gender}</p>
+        <p><strong>Height:</strong> {height}</p>
+        <p><strong>birth:</strong> {new Date(birth).toDateString()}</p>
+        
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default IdCard;
